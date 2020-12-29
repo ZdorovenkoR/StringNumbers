cd3f3d05002e15a269f4b00cafee6e1157daefaa
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-29 23:18:15 +0600

    Update Program.cs

--- StringNumbers/Program.cs
+++ StringNumbers/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Написать метод GetFullName(string firstName, string lastName, string
+//patronymic), принимающий на вход ФИО в разных аргументах и возвращающий
+//объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль
+//3–4 разных ФИО.
 namespace StringNumbers
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ряд чисел");
+            var userNumbers = Console.ReadLine();
+            Sum(SplitString(userNumbers));
+            Console.ReadKey();
+        }
+        static void Sum(string [] array)
+        {
+            int sumSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumSum += Convert.ToInt32(array[i]);
+            }
+            Console.WriteLine(sumSum);
+        }    
+
+        static string [] SplitString (string args)
+        {
+            return args.Split(' ');
         }
     }
+
 }
